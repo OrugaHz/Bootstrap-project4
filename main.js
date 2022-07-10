effb681ae3b46e3be1a565d5a8b2410da26a5ab2
Oruga <kanojodo@gmail.com>
2022-07-10 13:46:22 +0200

    changes, add: modal, cookie

--- main.js
+++ main.js
@@ -5,3 +5,26 @@ document.addEventListener('click', () => {
         nav.classList.remove('show')
     }
 })
+
+
+// obsługa cookies
+
+const cookieBox = document.querySelector('.cookie-box');
+const cookieBtn = document.querySelector('.cookie-btn');
+
+const showCookie = () => {
+    const cookieEaten = localStorage.getItem('cookie');
+    if (cookieEaten) {
+        cookieBox.classList.add('hide');
+    }
+}
+
+
+const handleCookieBox = () => {
+    localStorage.setItem('cookie', 'true');
+    cookieBox.classList.add('hide');
+}
+
+cookieBtn.addEventListener('click', handleCookieBox)
+
+showCookie()
